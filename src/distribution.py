1758ae4b119aa2e16ea3cffc5458ffbeec101f74
Tony <70nybl4nc0@gmail.com>
2020-12-08 03:26:38 -0500

    Clean up

--- src/distribution.py
+++ src/distribution.py
@@ -14,9 +14,5 @@ def exponential(lam: float) -> float:
     return 1 - e ** (-lam * random())
 
 
-def exponentialX(lam: float, x: float) -> float:
-    return 1 - e ** (-lam * x)
-
-
 def mean(value):
     return sum(value) / len(value)
